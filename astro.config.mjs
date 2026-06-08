7aa2c56059dd9ae86960e6a6637c177b3525046e
Chapman Koo <51713693+chapmankoo28@users.noreply.github.com>
2026-06-08 16:43:05 +0800

    chore: Add Tailwind CSS and React Compiler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: 'static',
+
+  vite: {
+		plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
 });

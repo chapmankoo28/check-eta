ddf35cf53c6e7309a4348a606ce083f3b8f3e9fe
Chapman Koo <51713693+chapmankoo28@users.noreply.github.com>
2026-06-10 23:58:38 +0800

    feat: Add vertical margins to main layout

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -3,6 +3,8 @@ import type { ReactNode } from 'react'
 
 export function Main({ className, children }: { className?: string; children: ReactNode }) {
   return (
-    <main className={cn('mx-auto w-full max-w-7xl px-24 max-sm:px-5', className)}>{children}</main>
+    <main className={cn('mx-auto my-2 w-full max-w-7xl px-24 max-sm:px-5 sm:my-5', className)}>
+      {children}
+    </main>
   )
 }

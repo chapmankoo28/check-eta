ad68b44e41fa2e8e46a2fd29375a3bd6ee61953f
Chapman Koo <51713693+chapmankoo28@users.noreply.github.com>
2026-06-12 22:16:57 +0800

    fix: redundant button in swap bound button

--- src/components/SwapBoundButton.tsx
+++ src/components/SwapBoundButton.tsx
@@ -1,5 +1,6 @@
-import { Button } from '@/components/ui/button'
+import { buttonVariants } from '@/components/ui/button'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
 import { ArrowsDownUpIcon } from '@phosphor-icons/react'
 
 export function SwapBoundButton({
@@ -11,15 +12,11 @@ export function SwapBoundButton({
 }) {
   return (
     <Tooltip>
-      <TooltipTrigger>
-        <Button
-          variant="secondary"
-          onClick={handleSwapBound}
-          size={'icon-sm'}
-          className={className}
-        >
-          <ArrowsDownUpIcon className={'text-foreground'} data-icon="inline-start" />
-        </Button>
+      <TooltipTrigger
+        onClick={handleSwapBound}
+        className={cn(buttonVariants({ variant: 'secondary', size: 'icon-sm' }), className)}
+      >
+        <ArrowsDownUpIcon className={'text-foreground'} data-icon="inline-start" />
       </TooltipTrigger>
       <TooltipContent>調轉方向</TooltipContent>
     </Tooltip>

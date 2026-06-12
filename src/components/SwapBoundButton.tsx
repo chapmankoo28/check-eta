89d2188f12af9662d56637ca22879fb09944d130
Chapman Koo <51713693+chapmankoo28@users.noreply.github.com>
2026-06-12 18:02:37 +0800

    feat: Make SwapBoundButton smaller

--- src/components/SwapBoundButton.tsx
+++ src/components/SwapBoundButton.tsx
@@ -3,7 +3,7 @@ import { ArrowsDownUpIcon } from '@phosphor-icons/react'
 
 export function SwapBoundButton({ handleSwapBound }: { handleSwapBound: () => void }) {
   return (
-    <Button variant="secondary" onClick={handleSwapBound}>
+    <Button variant="secondary" onClick={handleSwapBound} size={'sm'}>
       <ArrowsDownUpIcon className={'text-foreground'} data-icon="inline-start" />
       調轉方向
     </Button>
